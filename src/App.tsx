d0c9ec17d8bb0894411cb3891d015a70b40b0031
Gouseppe <fernandeitorc@gmail.com>
2024-03-15 09:16:08 -0400

    agregar el proveedor departments a app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import "dayjs/locale/es";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { ThemeProvider } from "./providers/theme";
-import { AuthProvider } from "./providers/auth";
-import { Router } from "./router/Router";
-import { ModalProvider } from "./providers/modal";
-import { EmployeesProvider } from "./providers/employees";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import 'dayjs/locale/es';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { ThemeProvider } from './providers/theme';
+import { AuthProvider } from './providers/auth';
+import { Router } from './router/Router';
+import { ModalProvider } from './providers/modal';
+import { EmployeesProvider } from './providers/employees';
+import { DeparmentsProvider } from './providers/deparments';
 
 function App() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='es'>
       <ThemeProvider>
         <AuthProvider>
           <ModalProvider>
             <EmployeesProvider>
-              <Router />
+              <DeparmentsProvider>
+                <Router />
+              </DeparmentsProvider>
             </EmployeesProvider>
           </ModalProvider>
         </AuthProvider>
