ad854e82612e6cb349fb8e8b64fe96d420729e80
JuanDiegoAR <arismendijuandiego70@gmail.com>
2024-03-19 13:50:42 -0400

    Inluiding the transformation of the data string to number

--- src/config/api/backend/requests/consult.ts
+++ src/config/api/backend/requests/consult.ts
@@ -10,6 +10,15 @@ export const editConsult = async ({
 }) => {
   const response = await backend.put(`consult/edit/${id}`, {
     ...data,
+    idCard: +data.idCard,
+    TAResult: +data.TAResult,
+    FCResult: +data.FCResult,
+    SPO2Result: +data.SPO2Result,
+    FRResult: +data.FRResult,
+    weight: +data.weight,
+    size: +data.size,
+    IMCResult: +data.IMCResult,
+    temperature: +data.temperature,
   });
 
   return response.status === 200;
@@ -18,6 +27,15 @@ export const editConsult = async ({
 export const createConsult = async ({ data }: { data: ConsultFormData }) => {
   const response = await backend.post("consult/create", {
     ...data,
+    idCard: +data.idCard,
+    TAResult: +data.TAResult,
+    FCResult: +data.FCResult,
+    SPO2Result: +data.SPO2Result,
+    FRResult: +data.FRResult,
+    weight: +data.weight,
+    size: +data.size,
+    IMCResult: +data.IMCResult,
+    temperature: +data.temperature,
   });
 
   return response.status === 201;
