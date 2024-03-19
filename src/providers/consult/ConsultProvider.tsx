ced5943b9b085f9d434a796fa2ce98b4d88aa5f4
JuanDiegoAR <arismendijuandiego70@gmail.com>
2024-03-19 12:47:43 -0400

    Creating the provider for Consult

---
+++ src/providers/consult/ConsultProvider.tsx
@@ -0,0 +1,51 @@
+import {
+  createConsult,
+  editConsult,
+} from "@app/config/api/backend/requests/consult";
+import { useRequest } from "@app/hooks/useRequest";
+import { ConsultFormData, ConsultState } from "@app/models/consult.model";
+import { FC, PropsWithChildren } from "react";
+import { ConsultContext } from "./ConsultContext";
+
+export const ConsultProvider: FC<PropsWithChildren> = ({ children }) => {
+  const _createOne = useRequest(createConsult);
+  const _editOne = useRequest(editConsult);
+
+  const createOne: ConsultState["createOne"] = async (
+    data: ConsultFormData
+  ) => {
+    const done = await _createOne({ data });
+
+    if (done) {
+      await getAll();
+
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const editOne: ConsultState["editOne"] = async (
+    id: string,
+    data: ConsultFormData
+  ) => {
+    const done = await _editOne({ data, id });
+
+    if (done) {
+      await getAll();
+
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const state: ConsultState = {
+    createOne,
+    editOne,
+  };
+
+  return (
+    <ConsultContext.Provider value={state}>{children}</ConsultContext.Provider>
+  );
+};
